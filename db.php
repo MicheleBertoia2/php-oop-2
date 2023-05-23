fd68c7d6c09af2f948a250a0ebb15b3420c54797
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-05-23 17:48:54 +0200

    [fix] implemented controls on printing elements

--- db.php
+++ db.php
@@ -5,7 +5,10 @@ require_once __DIR__ . '/models/House.php';
 require_once __DIR__ . '/models/ToY.php';
 
 $products = [
-  new Toy("papera di gomma","5.99","cat")
+  new Toy("papera di gomma","5.99","cat"),
+  new House("Cuccia Deluxe","24.99","dog")
 ];
+
+$products[0]->setMaterial('gomma');
 
 // var_dump($products);
--- views/layout/shop.php
+++ views/layout/shop.php
@@ -5,16 +5,19 @@
 
   <h3 class="mb-3">I nostri prodotti</h3>
 
-  <div class="row-cols-4">
+  <div class="row">
     <?php foreach($products as $product) :?>
-    <div class="col">
+    <div class="col-3">
       <div class="card">
         <img src="..." class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title"><?php echo $product->getTitle() ?></h5>
           <p><?php echo $product->getCategory()?></p>
           <p class="card-text"><?php echo $product->getDescription()?></p>
-          <p><?php echo $product->getPrice()?></p>
+          <p><?php echo $product->getPrice() . " €"?></p>
+          <p><?php if(property_exists($product, 'ingredients'))  echo $product->getIngredients() ?></p>
+          <p><?php if(property_exists($product, 'material'))  echo $product->getMaterial() ?></p>
+          <p><?php if(property_exists($product, 'weight'))  echo $product->getWeight()  ?></p>
         </div>
       </div>
     </div>
